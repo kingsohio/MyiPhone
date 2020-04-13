406e38dca5bdfd9a343c6422803fdaf7581eb04d
Kingsley Esezobor <koesezobo@aggies.ncat.edu>
2020-04-12 23:17:28 -0400

    Initial Commit

---
+++ MyiPhone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyiPhone
+//
+//  Created by MyMac on 4/12/20.
+//  Copyright © 2020 KCGroup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
